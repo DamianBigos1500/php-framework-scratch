f878e41eb549a32d02ce499979a3db38f24a357c
DamianBigos <damianbigos1500@gmail.com>
2023-06-18 18:20:05 +0200

    main: add folder structure with run file

---
+++ bootstrap/app.php
@@ -0,0 +1,23 @@
+<?php
+
+class Application
+{
+
+  /**
+   * Application constructor
+   * 
+   * */
+  private function __construct()
+  {
+  }
+
+  /**
+   * Run the application
+   * 
+   * @return void
+   * */
+  public static  function run()
+  {
+    echo "Running application";
+  }
+}
